cca55b3c413ec10b8353389db96cae1362fdf95a
xsahxl <xsahxl@126.com>
2023-06-25 11:06:51 +0800

    version: 0.0.2
    Signed-off-by: xsahxl <xsahxl@126.com>

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 
 export default class ComponentDemo {
-  private logger: any;
-  constructor({ logger }) {
-    this.logger = logger || console;
+  constructor(private logger = console) {
   }
   public async deploy(inputs) {
-
     this.logger.log(`deploy: ${JSON.stringify(inputs, null, 2)}`);
-
     return { hello: 'world' };
   }
 }
